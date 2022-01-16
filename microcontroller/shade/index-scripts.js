c77bdbd807b8b05501a67bf61a9c9b1ce83a3705
Olivier Mungo <olivier@mungo.eu>
2022-01-16 21:21:21 +0100

    .

--- microcontroller/shade/index-scripts.js
+++ microcontroller/shade/index-scripts.js
@@ -8,14 +8,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 function getValues() {
     fetchWithTimeout('/settings/values', {
-        timeout: 3000
+        timeout: 3000,
     })
-        .then(response => response.json())
-        .then(response => {
+        .then((response) => response.json())
+        .then((response) => {
             setTagValue('ip', response.ip);
             setTagValue('tag-net-id', response.netId);
+            setTagValue('reverse-motor', response.motorReversed);
 
             document.title = `${documentTitle} ${response.netId}`;
+
+            const appSpinner = document.getElementById('app-spinner');
+            appSpinner.classList.add('display-none');
+
+            const app = document.getElementById('app');
+            app.classList.remove('display-none');
         })
         .catch(() => setTimeout(getValues, 3000));
 }
